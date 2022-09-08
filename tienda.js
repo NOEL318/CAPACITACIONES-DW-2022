48b8dc538595301f5e2f728c9f1d7a821d76b0af
NOEL318 <noelrinaya318@gmail.com>
2022-09-08 18:29:32 -0500

    Sesiones completadas

---
+++ tienda.js
@@ -0,0 +1,12 @@
+//console.log("hello world 1");
+var cantidadseleccionada=0;
+ function funcion1() {
+    var cantidadseleccionada= document.getElementById("cantidad").value;
+    console.log("hello world funcion 1");
+    console.log(cantidadseleccionada);
+}
+
+var funcion2=()=>{
+    console.log("hello world funcion 2");
+    console.log(2);
+}
